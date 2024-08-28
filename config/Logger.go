4a1654e2b04d779b4b1dee63f73c2d7d5c7bea47
arifinlenna <arifin@lenna.ai>
2024-08-28 14:42:06 +0700

    update git ignore

--- config/Logger.go
+++ config/Logger.go
@@ -1,37 +1,121 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/sirupsen/logrus"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-func Logger(app *fiber.App) {
-	createDirStorageLogs()
-	log := &lumberjack.Logger{
-        Filename:   fmt.Sprintf("storage/logs/%v.log",time.Now().Format("2017-09-07")),
-        MaxSize:    1000000,  // Maximum size in megabytes before log is rotated
-        MaxBackups: 7,   // Maximum number of old log files to keep
-        MaxAge:     7,   // Maximum number of days to retain old log files
-        Compress:   true, // Compress old log files
+func setupLogger(filename string) *logrus.Logger {
+	// numberInt,_ := strconv.Atoi(os.Getenv("TIME_STORAGE_DAY"))
+
+	logFile, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatalf("Failed to open log file: %v", err)
+    }
+    defer logFile.Close()
+
+	// Set file permissions to 777
+    err = os.Chmod(filename, 0777)
+    if err != nil {
+        log.Fatalf("Failed to change file permissions: %v", err)
     }
 
-    loggerNew := logrus.New()
-    loggerNew.SetOutput(log)
-    loggerNew.SetFormatter(&logrus.JSONFormatter{})
+	lumberjackLogger := &lumberjack.Logger{
+		Filename:   filename,
+		MaxSize:    10000,
+		MaxBackups: 7,
+		MaxAge:    1,
+		Compress:   true,
+		LocalTime:  true,                        // Use local time for log rotation
+	}
 
-    app.Use(logger.New(logger.Config{
-        Output: loggerNew.Writer(), // Direct Fiber logs to the custom logger
-        Format: "${time} ${status} - ${method} ${path} ${body}\n", // Customize log format (optional)
-    }))
+	log := logrus.New()
+	log.SetOutput(lumberjackLogger)
+	log.SetFormatter(&logrus.JSONFormatter{})
+	return log
 }
+
+var GeneralLogger *logrus.Logger
+
+
+func Logger(app *fiber.App) {
+	createDirStorageLogs()
+
+	app.Use(func(c *fiber.Ctx) error {
+
+		date := time.Now().Format("01-02-2006")
+		generalLogFile := fmt.Sprintf("./storage/logs/general_log/%s.log", date)
+		dailyLogFile := fmt.Sprintf("./storage/logs/%s.log", date)
+
+		GeneralLogger = setupLogger(generalLogFile)
+		dailyLogger := setupLogger(dailyLogFile)
+
+
+		start := time.Now()
+		err := c.Next()
+		latency := time.Since(start)
+		latencyStr := fmt.Sprintf("%dms", latency.Milliseconds())
+
+		body := c.Body()
+		var bodyJSON interface{}
+		if err := json.Unmarshal(body, &bodyJSON); err == nil {
+			compactBody, _ := json.Marshal(bodyJSON)
+			c.Locals("body", string(compactBody))
+		} else {
+			c.Locals("body", string(body))
+		}
+
+		GeneralLogger.WithFields(logrus.Fields{
+			"body":          c.Locals("body"),
+			"queryParams":   c.OriginalURL(),
+			"reqHeaders":    c.GetReqHeaders(),
+			"time":          time.Now().Format("15:04:05"),
+			"date":          date,
+			"status":        c.Response().StatusCode(),
+			"ip":            c.IP(),
+			"method":        c.Method(),
+			"url":           c.OriginalURL(),
+			"path":          c.Path(),
+			"route":         c.Route().Path,
+			"error":         err,
+			"resBody":       string(c.Response().Body()),
+			"responseTime":  latencyStr,
+		}).Info("Request logged")
+
+		customLogEntry := fmt.Sprintf(
+			"body : %s | queryParams : %s | reqHeaders : %v | time : %s | date : %s | status : %d | ip : %s | method : %s | url : %s | path : %s | route : %s | error : %v | resBody : %s | responseTime : %s",
+			c.Locals("body"),
+			c.OriginalURL(),
+			c.GetReqHeaders(),
+			time.Now().Format("15:04:05"),
+			date,
+			c.Response().StatusCode(),
+			c.IP(),
+			c.Method(),
+			c.OriginalURL(),
+			c.Path(),
+			c.Route().Path,
+			err,
+			string(c.Response().Body()),
+			latencyStr,
+		)
+	
+		dailyLogger.Out.Write([]byte(customLogEntry + "\n"))
+
+		return nil
+	})
+}
+
 func createDirStorageLogs() {
 	dirs := []string{
+		"./storage/logs/general_log",
 		"./storage/logs",
 	}
 	for _, dir := range dirs {
